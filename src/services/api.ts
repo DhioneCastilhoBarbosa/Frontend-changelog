c59b0ab266b3c01b308b73d3b7a08c19f8d8e507
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2025-09-02 13:58:07 -0300

    feat: new URL for API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://changelog.intelbras-cve-pro.com.br/api", // Substitua pela URL da sua API
+  baseURL: "https://api-changelog.intelbras-cve-pro.com.br/api", // Substitua pela URL da sua API
 });
 
 api.interceptors.request.use((config) => {
